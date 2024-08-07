956f38b821a3d6afc054863294dff33b984c9301
tanvir-devolvedai <tanvir@devolvedai.com>
2024-08-07 15:13:09 +0600

    monitor code restored

--- frontend/monitor_and_restart.sh
+++ frontend/monitor_and_restart.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 LOG_FILE="./logs/mexc_indexing_frontend-error.log"
+ERROR_KEYWORDS=("Invariant: Missing 'next-action' header" "Failed to find Server Action" "node:async_hooks")
 
 while true; do
-    # Check if the log file is not empty
-    if [ -s "$LOG_FILE" ]; then
-        echo "Error detected in log file."
-        echo "Executing recovery steps..."
-        pm2 restart mexc_indexing_frontend
-        echo -n > /logs/mexc_indexing_frontend-error.log
+    for keyword in "${ERROR_KEYWORDS[@]}"; do
+        if grep -q "$keyword" "$LOG_FILE"; then
+            echo "Error detected: $keyword"
+            echo "Executing recovery steps..."
+            rm -rf .next
+            npm run build
+            pm2 restart mexc_indexing_frontend
 
-        # Clear the log file to prevent repeated restarts on the same error
-        : > "$LOG_FILE"
-    fi
+            # Clear the log file to prevent repeated restarts on the same error
+            : > "$LOG_FILE"
+        fi
+    done
     sleep 30  # Check every 30 seconds
 done
