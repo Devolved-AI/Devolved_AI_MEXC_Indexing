9948d21a565fcffa4b26b6a3ff046ac57d3dacb9
tanvir-devolvedai <tanvir@devolvedai.com>
2024-08-07 12:28:43 +0600

    removed log filter

--- frontend/monitor_and_restart.sh
+++ frontend/monitor_and_restart.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
 LOG_FILE="./logs/mexc_indexing_frontend-error.log"
-# ERROR_KEYWORDS=("Invariant: Missing 'next-action' header" "Failed to find Server Action" "node:async_hooks")
 
 while true; do
-    for keyword in "${ERROR_KEYWORDS[@]}"; do
-        if grep -q "$keyword" "$LOG_FILE"; then
-            echo "Error detected in log file."
-            echo "Executing recovery steps..."
-            rm -rf .next
-            npm run build
-            pm2 restart mexc_indexing_frontend
+    # Check if the log file is not empty
+    if [ -s "$LOG_FILE" ]; then
+        echo "Error detected in log file."
+        echo "Executing recovery steps..."
+        rm -rf .next
+        npm run build
+        pm2 restart mexc_indexing_frontend
 
-            # Clear the log file to prevent repeated restarts on the same error
-            : > "$LOG_FILE"
-        fi
-    done
+        # Clear the log file to prevent repeated restarts on the same error
+        : > "$LOG_FILE"
+    fi
     sleep 30  # Check every 30 seconds
 done
