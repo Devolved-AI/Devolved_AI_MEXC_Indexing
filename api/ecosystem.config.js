764b59b0611b6338d7ed1c3f5553524e8a17f505
tanvir-devolvedai <tanvir@devolvedai.com>
2024-08-11 13:30:56 +0600

    fixed ecosystemjs

--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
     apps: [
       {
         name: 'mexc-indexing-api',
-        script: "node",
-        args: "src/server.js",
+        script: "npm",
+        args: "start",
         instances: 1,
         autorestart: true,
         time: true,
@@ -12,26 +12,24 @@ module.exports = {
         ignore_watch : ["node_modules", "logs"],
         max_memory_restart: "1G",
         log_date_format: "YYYY-MM-DD HH:mm Z",
-        error_file: './logs/fetchChainData-error.log',
-        out_file: './logs/fetchChainData-out.log',
+        error_file: './logs/mexc-indexing-api-error.log',
+        out_file: './logs/mexc-indexing-api-out.log',
         combine_logs: true,
         env: {
           NODE_ENV: 'development',
           POSTGRES_DB: process.env.POSTGRES_DB,
           POSTGRES_USER: process.env.POSTGRES_USER,
           POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
-          DATABASE_URL: process.env.DATABASE_URL,
-          ARGOCHAIN_RPC_URL: process.env.ARGOCHAIN_RPC_URL,
-          FETCHING_BATCH_SIZE: process.env.FETCHING_BATCH_SIZE,
+          POSTGRES_HOST: process.env.POSTGRES_HOST,
+          POSTGRES_PORT: process.env.POSTGRES_PORT,
         },
         env_production: {
           NODE_ENV: 'production',
           POSTGRES_DB: process.env.POSTGRES_DB,
           POSTGRES_USER: process.env.POSTGRES_USER,
           POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
-          DATABASE_URL: process.env.DATABASE_URL,
-          ARGOCHAIN_RPC_URL: process.env.ARGOCHAIN_RPC_URL,
-          FETCHING_BATCH_SIZE: process.env.FETCHING_BATCH_SIZE,
+          POSTGRES_HOST: process.env.POSTGRES_HOST,
+          POSTGRES_PORT: process.env.POSTGRES_PORT,
         }
       }
     ]
