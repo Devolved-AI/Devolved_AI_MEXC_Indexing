4ee155b142d1697ad3875c26fe779f330d5fe139
tanvir-devolvedai <tanvir@devolvedai.com>
2024-10-03 14:46:51 +0000

    restored previous data

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -1,24 +1,41 @@
 module.exports = {
   apps: [
     {
-      name: "mexc_indexing_frontend",
+      name: "argochain-scanner-frontend",
       script: "npm",
       args: "start",
       autorestart: true,
-      // max_restarts: 100,
       restart_delay: 3000,
       watch: true,
       ignore_watch: ["node_modules", "logs"],
       max_memory_restart: "1G",
       log_date_format: "YYYY-MM-DD HH:mm Z",
-      error_file: './logs/mexc_indexing_frontend-error.log',
-      out_file: './logs/mexc_indexing_frontend-out.log',
+      error_file: './logs/argochain-scanner-frontend.log',
+      out_file: './logs/argochain-scanner-frontend.log',
       combine_logs: true,
       watch_options: {
         followSymlinks: false,
         usePolling: true,
         interval: 1000,
       },
+      env: {
+        NODE_ENV: 'development',
+        POSTGRES_DB: process.env.POSTGRES_DB,
+        POSTGRES_USER: process.env.POSTGRES_USER,
+        POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+        POSTGRES_HOST: process.env.POSTGRES_HOST,
+        POSTGRES_PORT: process.env.POSTGRES_PORT,
+        PORT: process.env.PORT
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        POSTGRES_DB: process.env.POSTGRES_DB,
+        POSTGRES_USER: process.env.POSTGRES_USER,
+        POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+        POSTGRES_HOST: process.env.POSTGRES_HOST,
+        POSTGRES_PORT: process.env.POSTGRES_PORT,
+        PORT: process.env.PORT
+      }
     },
   ],
 };
